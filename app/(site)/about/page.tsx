50fdf24523d389c44e8b43598648bbf7e7ebe379
3narinder <narinderd9@gmail.com>
2025-03-21 14:05:39 +0530

    about page

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const About = () => {
+  return (
+    <section className="lg:px-32 px-6 py-6">
+      <div className="flex flex-col gap-2 items-center justify-center">
+        <h1 className="heading-7 tracking-wider font-semibold uppercase">
+          about moon
+        </h1>
+
+        <div className="text-display-2 text-neutral-6 w-96 text-center">
+          Moon's Fashion clothing products have been around since 1650, let's
+          explore our journey
+        </div>
+      </div>
+    </section>
+  );
 };
 
-export default page;
+export default About;
